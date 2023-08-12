d384272a413ea969be0b0b66da6b051a6f3f7e8a
Michael Erwin <michael.erwinp@gmail.com>
2023-08-12 20:29:42 +0800

    Missing route.

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('products', [ProductController::class, 'index']);
-Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
 Route::patch('products/{id}', [ProductController::class, 'update']);
 Route::post('products', [ProductController::class, 'store']);
 Route::delete('products/{id}', [ProductController::class, 'destroy']);
